45760e7b502da0db5011f27f6d2c4659795462ec
Aitor <aitirga@gmail.com>
2024-01-17 14:46:31 +0100

    refactor: test stuff related to BC

--- src/pflotran/general.F90
+++ src/pflotran/general.F90
@@ -1635,7 +1635,7 @@ subroutine GeneralResidual(snes,xx,r,realization,ierr)
 
       local_end = local_id * option%nflowdof
       local_start = local_end - option%nflowdof + 1
-      r_p(local_start:local_end)= r_p(local_start:local_end) - Res(:)
+      r_p(local_start:local_end)= 0! r_p(local_start:local_end) - Res(:)*vol_frac_prim
 !      r_p(local_end-1)= r_p(local_end-1) - Res()
 
     enddo
