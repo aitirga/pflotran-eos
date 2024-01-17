861e1213e61bf32d813baee67f5148b040c4bdbc
Aitor <aitirga@gmail.com>
2024-01-17 17:23:44 +0100

    refactor: test new location

--- src/pflotran/general.F90
+++ src/pflotran/general.F90
@@ -1618,7 +1618,7 @@ subroutine GeneralResidual(snes,xx,r,realization,ierr)
                      local_id == general_debug_cell_id)
       patch%boundary_velocities(:,sum_connection) = v_darcy * vol_frac_prim
       if (associated(patch%boundary_flow_fluxes)) then
-        patch%boundary_flow_fluxes(:,sum_connection) = Res(:)
+        patch%boundary_flow_fluxes(:,sum_connection) = Res(:) * vol_frac_prim
       endif
       if (option%compute_mass_balance_new) then
         ! contribution to boundary
