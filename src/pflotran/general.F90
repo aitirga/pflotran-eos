c688afb90d246fe962dbdc8e8b1b1d7ebaabb515
Aitor <aitirga@gmail.com>
2024-01-23 17:56:35 +0100

    refactor: test

--- src/pflotran/general.F90
+++ src/pflotran/general.F90
@@ -1636,7 +1636,7 @@ subroutine GeneralResidual(snes,xx,r,realization,ierr)
 
       local_end = local_id * option%nflowdof
       local_start = local_end - option%nflowdof + 1
-      r_p(local_start:local_end)= r_p(local_start:local_end) - Res(:)*vol_frac_prim
+      r_p(local_start:local_end)= r_p(local_start:local_end) - Res(:)
 
     enddo
     boundary_condition => boundary_condition%next
